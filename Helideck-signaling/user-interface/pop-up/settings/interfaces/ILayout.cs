23da94f96c79ba3784cf86071263f92990757f15
niziul <NiziulLuizin@Gmail.com>
2024-05-04 22:36:15 -0300

    chore: cleaning up old UI implementation

--- Helideck-signaling/user-interface/pop-up/settings/interfaces/ILayout.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System;
-
-//namespace Helideck_Signaling.user_interface.pop_up.settings.interfaces
-//{
-//    internal interface ILayout
-//    {
-//        void DisposeAll();
-//    }
-//}
